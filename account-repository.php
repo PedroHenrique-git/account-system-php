1da6a946312f65a0a9437db7c9e5b52739a8d89b
PedroHenrique-git <pedromf34@outlook.com>
2023-05-01 22:30:00 -0300

    fix: account repository

--- account-repository.php
+++ account-repository.php
@@ -5,5 +5,6 @@
         public function createAccount(Account $account);
         public function listAccounts();
         public function deposit($document, $password, $amount);
+        public function withdraw($document, $password, $amount);
         public function transference(Account $sender, Account $receiver);
     }
